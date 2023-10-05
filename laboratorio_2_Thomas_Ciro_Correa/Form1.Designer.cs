c4eada3ec2994c56b29e455823c0eea47b5fbe81
Thomas Ciro <thomyciro@gmail.com>
2023-10-05 01:20:44 -0500

    Creacion de la interfaz del formulario

---
+++ laboratorio_2_Thomas_Ciro_Correa/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace laboratorio_2_Thomas_Ciro_Correa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTitulo = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.labelRecta1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.labelRecta2 = new System.Windows.Forms.Label();
+            this.labelError = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.botonCalcular = new System.Windows.Forms.Button();
+            this.helpProvider1 = new System.Windows.Forms.HelpProvider();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelTitulo
+            // 
+            this.labelTitulo.AutoSize = true;
+            this.labelTitulo.Cursor = System.Windows.Forms.Cursors.Default;
+            this.labelTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitulo.Location = new System.Drawing.Point(380, 20);
+            this.labelTitulo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelTitulo.Name = "labelTitulo";
+            this.labelTitulo.Size = new System.Drawing.Size(68, 16);
+            this.labelTitulo.TabIndex = 0;
+            this.labelTitulo.Text = "RECTAS";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(148, 52);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(116, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // labelRecta1
+            // 
+            this.labelRecta1.AutoSize = true;
+            this.labelRecta1.Location = new System.Drawing.Point(53, 24);
+            this.labelRecta1.Name = "labelRecta1";
+            this.labelRecta1.Size = new System.Drawing.Size(45, 13);
+            this.labelRecta1.TabIndex = 2;
+            this.labelRecta1.Text = "Recta 1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(19, 52);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(113, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // labelRecta2
+            // 
+            this.labelRecta2.AutoSize = true;
+            this.labelRecta2.Location = new System.Drawing.Point(179, 24);
+            this.labelRecta2.Name = "labelRecta2";
+            this.labelRecta2.Size = new System.Drawing.Size(45, 13);
+            this.labelRecta2.TabIndex = 4;
+            this.labelRecta2.Text = "Recta 2";
+            // 
+            // labelError
+            // 
+            this.labelError.AutoSize = true;
+            this.labelError.Location = new System.Drawing.Point(28, 292);
+            this.labelError.MaximumSize = new System.Drawing.Size(300, 0);
+            this.labelError.Name = "labelError";
+            this.labelError.Size = new System.Drawing.Size(228, 13);
+            this.labelError.TabIndex = 5;
+            this.labelError.Text = "LAS RECTAS LAS DEBE3 INGRESAR COMO";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.botonCalcular);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.labelRecta2);
+            this.groupBox1.Controls.Add(this.labelRecta1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 101);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(293, 150);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.button2.Location = new System.Drawing.Point(172, 93);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(76, 30);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Limpiar";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // botonCalcular
+            // 
+            this.botonCalcular.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.botonCalcular.Location = new System.Drawing.Point(35, 93);
+            this.botonCalcular.Name = "botonCalcular";
+            this.botonCalcular.Size = new System.Drawing.Size(73, 30);
+            this.botonCalcular.TabIndex = 5;
+            this.botonCalcular.Text = "Calcular";
+            this.botonCalcular.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(794, 403);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.labelError);
+            this.Controls.Add(this.labelTitulo);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTitulo;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label labelRecta1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label labelRecta2;
+        private System.Windows.Forms.Label labelError;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button botonCalcular;
+        private System.Windows.Forms.HelpProvider helpProvider1;
+    }
+}
+
