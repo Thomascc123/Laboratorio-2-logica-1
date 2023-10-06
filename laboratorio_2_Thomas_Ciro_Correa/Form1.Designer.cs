37f2e57cfbc71b9e535782f82f8fadd34af81cd5
Thomas Ciro <thomyciro@gmail.com>
2023-10-05 23:39:13 -0500

    crecacion de las funciones necesarias para las rectas

--- laboratorio_2_Thomas_Ciro_Correa/Form1.Designer.cs
+++ laboratorio_2_Thomas_Ciro_Correa/Form1.Designer.cs
@@ -37,10 +37,7 @@
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.botonLimpiar = new System.Windows.Forms.Button();
             this.botonCalcular = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pruebas = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // labelTitulo
@@ -48,42 +45,45 @@
             this.labelTitulo.AutoSize = true;
             this.labelTitulo.Cursor = System.Windows.Forms.Cursors.Default;
             this.labelTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelTitulo.Location = new System.Drawing.Point(380, 20);
-            this.labelTitulo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelTitulo.Location = new System.Drawing.Point(507, 25);
             this.labelTitulo.Name = "labelTitulo";
-            this.labelTitulo.Size = new System.Drawing.Size(68, 16);
+            this.labelTitulo.Size = new System.Drawing.Size(82, 20);
             this.labelTitulo.TabIndex = 0;
             this.labelTitulo.Text = "RECTAS";
             // 
             // textBoxRecta2
             // 
-            this.textBoxRecta2.Location = new System.Drawing.Point(153, 52);
+            this.textBoxRecta2.Location = new System.Drawing.Point(204, 64);
+            this.textBoxRecta2.Margin = new System.Windows.Forms.Padding(4);
             this.textBoxRecta2.Name = "textBoxRecta2";
-            this.textBoxRecta2.Size = new System.Drawing.Size(120, 20);
+            this.textBoxRecta2.Size = new System.Drawing.Size(159, 22);
             this.textBoxRecta2.TabIndex = 1;
             // 
             // labelRecta1
             // 
             this.labelRecta1.AutoSize = true;
-            this.labelRecta1.Location = new System.Drawing.Point(53, 24);
+            this.labelRecta1.Location = new System.Drawing.Point(71, 30);
+            this.labelRecta1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelRecta1.Name = "labelRecta1";
-            this.labelRecta1.Size = new System.Drawing.Size(45, 13);
+            this.labelRecta1.Size = new System.Drawing.Size(53, 16);
             this.labelRecta1.TabIndex = 2;
             this.labelRecta1.Text = "Recta 1";
             // 
             // textBoxRecta1
             // 
-            this.textBoxRecta1.Location = new System.Drawing.Point(10, 52);
+            this.textBoxRecta1.Location = new System.Drawing.Point(13, 64);
+            this.textBoxRecta1.Margin = new System.Windows.Forms.Padding(4);
             this.textBoxRecta1.Name = "textBoxRecta1";
-            this.textBoxRecta1.Size = new System.Drawing.Size(120, 20);
+            this.textBoxRecta1.Size = new System.Drawing.Size(159, 22);
             this.textBoxRecta1.TabIndex = 3;
             // 
             // labelRecta2
             // 
             this.labelRecta2.AutoSize = true;
-            this.labelRecta2.Location = new System.Drawing.Point(186, 24);
+            this.labelRecta2.Location = new System.Drawing.Point(248, 30);
+            this.labelRecta2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelRecta2.Name = "labelRecta2";
-            this.labelRecta2.Size = new System.Drawing.Size(45, 13);
+            this.labelRecta2.Size = new System.Drawing.Size(53, 16);
             this.labelRecta2.TabIndex = 4;
             this.labelRecta2.Text = "Recta 2";
             // 
@@ -91,10 +91,11 @@
             // 
             this.labelError.AutoSize = true;
             this.labelError.Font = new System.Drawing.Font("Microsoft JhengHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelError.Location = new System.Drawing.Point(37, 243);
-            this.labelError.MaximumSize = new System.Drawing.Size(290, 0);
+            this.labelError.Location = new System.Drawing.Point(566, 110);
+            this.labelError.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelError.MaximumSize = new System.Drawing.Size(387, 0);
             this.labelError.Name = "labelError";
-            this.labelError.Size = new System.Drawing.Size(243, 136);
+            this.labelError.Size = new System.Drawing.Size(311, 176);
             this.labelError.TabIndex = 5;
             this.labelError.Text = "INGRESE DOS RECTAS DE LA FORMA \r\n\r\n              y – y1 = m (x – x1)   Ó\r\n       " +
     "            y = mx + b \r\n\r\nPARA CALCULAR SU INTERSECCIÓN,\r\nSI ES NECESARIO REEMP" +
@@ -109,67 +110,51 @@
             this.groupBox1.Controls.Add(this.labelRecta2);
             this.groupBox1.Controls.Add(this.labelRecta1);
             this.groupBox1.Controls.Add(this.textBoxRecta2);
-            this.groupBox1.Location = new System.Drawing.Point(34, 82);
+            this.groupBox1.Location = new System.Drawing.Point(45, 101);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(293, 150);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(391, 185);
             this.groupBox1.TabIndex = 6;
             this.groupBox1.TabStop = false;
             // 
             // botonLimpiar
             // 
             this.botonLimpiar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.botonLimpiar.Location = new System.Drawing.Point(172, 93);
+            this.botonLimpiar.Location = new System.Drawing.Point(229, 114);
+            this.botonLimpiar.Margin = new System.Windows.Forms.Padding(4);
             this.botonLimpiar.Name = "botonLimpiar";
-            this.botonLimpiar.Size = new System.Drawing.Size(76, 30);
+            this.botonLimpiar.Size = new System.Drawing.Size(101, 37);
             this.botonLimpiar.TabIndex = 6;
             this.botonLimpiar.Text = "Limpiar";
             this.botonLimpiar.UseVisualStyleBackColor = false;
+            this.botonLimpiar.Click += new System.EventHandler(this.botonLimpiar_Click);
             // 
             // botonCalcular
             // 
             this.botonCalcular.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.botonCalcular.Location = new System.Drawing.Point(35, 93);
+            this.botonCalcular.Location = new System.Drawing.Point(47, 114);
+            this.botonCalcular.Margin = new System.Windows.Forms.Padding(4);
             this.botonCalcular.Name = "botonCalcular";
-            this.botonCalcular.Size = new System.Drawing.Size(73, 30);
+            this.botonCalcular.Size = new System.Drawing.Size(97, 37);
             this.botonCalcular.TabIndex = 5;
             this.botonCalcular.Text = "Calcular";
             this.botonCalcular.UseVisualStyleBackColor = false;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.pictureBox1.Location = new System.Drawing.Point(444, 82);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(280, 280);
-            this.pictureBox1.TabIndex = 7;
-            this.pictureBox1.TabStop = false;
-            // 
-            // pruebas
-            // 
-            this.pruebas.AutoSize = true;
-            this.pruebas.Location = new System.Drawing.Point(532, 43);
-            this.pruebas.Name = "pruebas";
-            this.pruebas.Size = new System.Drawing.Size(35, 13);
-            this.pruebas.TabIndex = 8;
-            this.pruebas.Text = "label1";
-            this.pruebas.Click += new System.EventHandler(this.pruebas_Click);
+            this.botonCalcular.Click += new System.EventHandler(this.botonCalcular_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(794, 403);
-            this.Controls.Add(this.pruebas);
-            this.Controls.Add(this.pictureBox1);
+            this.ClientSize = new System.Drawing.Size(1059, 496);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.labelError);
             this.Controls.Add(this.labelTitulo);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -186,8 +171,6 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button botonLimpiar;
         private System.Windows.Forms.Button botonCalcular;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Label pruebas;
     }
 }
 
--- laboratorio_2_Thomas_Ciro_Correa/Form1.cs
+++ laboratorio_2_Thomas_Ciro_Correa/Form1.cs
@@ -12,10 +12,83 @@ namespace laboratorio_2_Thomas_Ciro_Correa
 {
     public partial class Form1 : Form
     {
+        Graphics g;
+        Pen p;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        public void reestablecer() {
+            textBoxRecta1.Text = "";
+            textBoxRecta2.Text = "";
+
+            labelError.Text = "INGRESE DOS RECTAS DE LA FORMA " +
+                "\r\n\r\n              y – y1 = m (x – x1)   Ó" +
+                "\r\n                   y = mx + b " +
+                "\r\n\r\nPARA CALCULAR SU INTERSECCIÓN," +
+                "\r\nSI ES NECESARIO REEMPLACE LOS " +
+                "\r\n\"-\" POR \"+\" Y VICEVERSA\r\n";
+        }
+
+        public void Error() {
+            reestablecer();
+            labelError.Text = "ERROR: por favor ingrese las rectas de la siguiente forma:" +
+                "\r\n\r\n              y – y1 = m(x – x1)   Ó" +
+                "\r\n                   y = mx + b ";
+        }
+
+        /*
+            *metodo validadNumero
+            *Parametros: String num
+            *
+            *valida si num se puede convertir en un numero
+            *
+            *retorna el string "error" sino se puede volver un numero, retorna num si si se puede convertir
+        */
+
+        public String validarNumero(String num) {
+            float aux = 0;
+            if (float.TryParse(num, out aux))
+            {
+                return num;
+            }
+            else {
+                return "error";
+            }
+            
+        }
+        /*
+            *metodo calcularValorIntersecto
+            *parametros: String y1, String x1, String pendiente
+            *
+            *valida si los datos ingresados se pueden convertir a numeros, de ser asi calcula el intersecto con el eje y
+            *
+            *retorna un string que contiene el intersecto con el eje y
+            *
+        */
+        public String calcularValorIntersecto(String y1, String x1, String pendiente)
+        {
+            if (validarNumero(y1) == "error" || validarNumero(x1) == "error" || validarNumero(pendiente) == "error")
+            {
+                return "error";
+            }
+            else
+            {
+                float numX1 = float.Parse(x1);
+                float numY1 = float.Parse(y1);
+                float numPendiente = float.Parse(pendiente);
+
+                float numIntersecto = (numPendiente * numX1) + (-1 * numY1);
+
+                String intersecto = String.Concat("", numIntersecto);
+
+                return intersecto;
+            }
+
+        }
+    
 
         /*
             * metodo recortarString 
@@ -31,35 +104,6 @@ namespace laboratorio_2_Thomas_Ciro_Correa
             texto = texto.Replace(".",",");
 
             return texto;
-        }
-
-        /*
-            *metodo tipoEcuacion 
-            *Parametros: String ecuacion
-            *
-            *valida un tipo de error en ecuacion y si es una ecuacion punto intercepto o punto pendiente
-            *
-            *devuelve un String que indica que tipo de ecuacion es o si es un error
-        */
-
-        public String validarTipoEcuacion(String ecuacion) {
-
-            if (ecuacion[0] != "y"[0])
-            {
-                return "error";
-            }
-            else if (ecuacion[1] == "-"[0] || ecuacion[1] == "+"[0])
-            {
-                return "pendiente";
-            }
-            else if (ecuacion[1] == "="[0])
-            {
-                return "intercepto";
-            }
-            else { 
-                return "error";
-            }
-
         }
 
 
@@ -84,8 +128,7 @@ namespace laboratorio_2_Thomas_Ciro_Correa
             String pendiente = "";
             String intersecto = "";
 
-
-            if (posicionX < 2 || posicionIgual != 1) {
+            if (posicionX < 2 || posicionIgual != 1 ) {
                 String[] error = { "error","error" };
                 return error;
             }
@@ -122,54 +165,271 @@ namespace laboratorio_2_Thomas_Ciro_Correa
                 pendiente = "0";
                 intersecto = ecuacion.Substring(posicionIgual, tamanioInterseccion);
             }
+
+            pendiente = pendiente.Replace("+","");
+            intersecto = intersecto.Replace("+", "");
+
             String[] datos = { pendiente, intersecto };
 
             return datos;
         }
 
+
+        /*
+            *metodo datosPendiente
+            *Parametros: String ecuacion
+            *
+            *valida si el string ecuacion tiene algunos errores, sino busca la pendiente y saca el intersecto con el eje y
+            *
+            *retorna un vector string que contiene a la pendiente y al interseccto, en caso de algun error el vector tendra dos string con "error"
+        */
         public String[] datosPendiente(String ecuacion) {
             // casos posibles: y +- y1 = m(x +- x1) || y +- y1 = mx || y +- y1 = x +- x1 || y +- y1 = 0 || y =  m(x +- x1) || y = x +- x1
-            // (los otros casos entran en la otra funcion)
-
+            // (otros casos entran en la funcion datosIntercepto)
+            Console.WriteLine("holaaaaaa");
             int posicionY = ecuacion.IndexOf("y");
             int posicionX = ecuacion.IndexOf("x");
             int posicionIgual = ecuacion.IndexOf("=");
-            0
-            if (posicionY != 0 || posicionIgual == -1 || posicionX == -1 || posicionX < posicionIgual || (ecuacion[1] != "+"[0] && ecuacion[1] != "-"[0] && ecuacion[1] != "="[0]) ) { 
+            int posicionParentesisInicial = ecuacion.IndexOf("(");
+            int posicionParentesisFinal = ecuacion.IndexOf(")");
+            int tamanioEcuacion = ecuacion.Length;
+
+            Console.WriteLine(ecuacion);
+
+            String y1 = "";
+            String x1 = "";
+            String pendiente = "";
+
+            Console.WriteLine(ecuacion+" dentro de pendiente");
             
+            if (posicionY != 0 || posicionIgual == -1 || (posicionX == -1 && ecuacion[posicionIgual+1] != "0"[0]) || (posicionX != -1 && posicionX < posicionIgual) || posicionIgual == 2 || //validando que exista "y", que exista "x" si m != 0, que exista "=", que "x" este despues del "=", que no se de esto y - = m( x - x1) 
+                (ecuacion[posicionY+1] != "+"[0] && ecuacion[posicionY + 1] != "-"[0] && ecuacion[posicionY + 1] != "="[0]) || // validando que despues del "y" este "+", "-" o "="
+                (posicionParentesisInicial != -1 && posicionParentesisFinal == -1) || (posicionParentesisInicial == -1 && posicionParentesisFinal != -1) || // validando que no pase y - y1 = m(x - x1    Ó   y - y1 = mx - x1)
+                (posicionParentesisInicial != -1 && posicionParentesisInicial+1 != posicionX) ||   // validando que si existe "(", justo antes de x
+                (posicionParentesisInicial == -1 && posicionParentesisFinal == -1 && posicionX != tamanioEcuacion-1 && ecuacion[posicionX+1] != "+"[0] && ecuacion[posicionX+1] != "-"[0]) ||  
+                (posicionParentesisFinal != -1 && posicionParentesisFinal != tamanioEcuacion-1) ||  // validando que si existe ")", este al final
+                (posicionX != -1 && ( ecuacion[posicionX+1] == "-"[0]  || ecuacion[posicionX + 1] == "+"[0] ) && (posicionX+1 == posicionParentesisFinal  || posicionX +1 == tamanioEcuacion-1))  ) // validando que no pase y - y1 = mx -   Ó   y - y1 = m(x - )
+            {
+
+                Console.WriteLine(" xd si entra " + ecuacion);
+                String[] error = { "error" , "error" };
+                return error;
             }
 
+            if (ecuacion[posicionIgual+1] == "("[0] ) // si la ecuacion es de la forma y +- y1 = (x +- x1) le quita los parentedsis
+            {
+                ecuacion.Replace("(","");
+                ecuacion.Replace(")","");
+            }
 
-            String[] datos = { };
+            // obtener y1
+            if (ecuacion[posicionY + 1] == "+"[0] || ecuacion[posicionY + 1] == "-"[0])
+            {
+                y1 = ecuacion.Substring(posicionY + 1, posicionIgual-1);
+            }
+            else {
+                Console.WriteLine(ecuacion+ "y es cerooo");
+                y1 = "0";
+            }
+
+            // obtener m y x1
+            if (ecuacion[posicionIgual+1] != "x"[0]  && ecuacion[posicionIgual + 1] != "0"[0]) {
+                
+                pendiente = ecuacion.Substring(posicionIgual+1 , posicionParentesisInicial - posicionIgual - 1);
+
+                if (posicionParentesisFinal == tamanioEcuacion - 1) // si tiene parentesis, x1 esta despues de x
+                {
+                    x1 = ecuacion.Substring( posicionX+1 , tamanioEcuacion - 2 - posicionX  );
+                }
+                else  // si no tiene parentesis, x1 es 0
+                {
+                    x1 = "0";
+                }
+
+            }
+            else{
+
+                if (ecuacion[posicionIgual + 1] == "0"[0]) // si la parte derecha de la igualdad es 0, la pendiente es cero y x1 es 0
+                {
+                    pendiente = "0";
+                    x1 = "0";
+
+                }
+                else
+                {  // si la parte derecha no tiene pendiente pero no es cero, la pendiente es 1
+                    pendiente = "1";
+
+                    if (posicionX == tamanioEcuacion - 1) // si x1 no aparece en la ecuaciones cero.
+                    {
+                        x1 = "0";
+                    }
+                    else //si entra aqui es porque x1 esta en la ecuacion, entonces es diferente de cero
+                    {
+                        x1 = ecuacion.Substring(posicionX + 1, tamanioEcuacion - 1 - posicionX);
+                    }
+                }
+
+            }
+
+           
+
+            pendiente = pendiente.Replace("+", "");
+            x1 = x1.Replace("+", "");
+            y1 = y1.Replace("+", "");
+            
+            Console.WriteLine(ecuacion+"  x1-> "+x1+"  y1-> "+y1+ " pendiente "+ pendiente);
+
+            String intersecto = calcularValorIntersecto(y1, x1, pendiente);
+
+            String[] datos = { pendiente , intersecto};
             return datos;
         }
 
-        public float extraerDatos(String ecuacion) {
-            ecuacion = recortarString(ecuacion);
 
-            String tipoEcuacion = validarTipoEcuacion(ecuacion);
-            switch (tipoEcuacion) {
-                case "intercepto":
+        /*
+            *metodo calcPuntoInterseccion 
+            *parametros: float pendiente1, float pendiente2, float intersecto1, float intersecto2
+            *
+            *con los datos de las rectas pedidos calcula el punto de interseccion entre estas.
+            *
+            *retorna un vector de flotantes que tiene la coordenada x y coordenada y del punto de interseccion
+        */
+        public float[] calcPuntoInterseccion(float pendiente1, float pendiente2, float intersecto1, float intersecto2) { 
 
-                    break;
-                case "pendiente":
+            float coordendaX = (intersecto2-intersecto1)/(pendiente1-pendiente2);
+            float coordenadaY = pendiente1 * coordendaX + intersecto1;
 
-                    break;
-                case "error":
+            float[] puntoInterseccion = { coordendaX, coordenadaY };
 
-                    break;
-            }
-
-            return 0;
+            return puntoInterseccion;
         }
 
-        private void pruebas_Click(object sender, EventArgs e)
+
+        /*
+            *metodo identificarCasos
+            *Parametos: float pendiente1, float intersecto1, float pendiente2, float intersecto2
+            *
+            *usa los datos de las rectas para identificar si estas son iguales, paralelas, perpendiculares o si se cruzan
+            *
+            *retorna un string que indica uno de los cuatro casos anteriores
+            *
+         */
+        public String identidicarCaso(float pendiente1, float intersecto1, float pendiente2, float intersecto2) {
+            String caso = "";
+            if (pendiente1 == pendiente2 ) {
+                if (intersecto1 == intersecto2)
+                {
+                    caso = "iguales";
+                }
+                else {
+                    caso = "paralelas";
+                }
+            }
+            else
+            {
+                if (pendiente1*pendiente2 == -1)
+                {
+                    caso = "perpendiculares";
+                }
+                else
+                {
+                    caso = "cruzan";
+                }
+            }
+
+            return caso;
+        }
+
+        public void calcularRectas(String ec1, String ec2) {
+
+            //encontrando datos de la recta 1
+
+            String[] datosEc1 = datosIntercepto(ec1);
+
+            if (validarNumero(datosEc1[0]) == "error" || validarNumero(datosEc1[1]) == "error")
+            {
+                datosEc1 = datosPendiente(ec1);
+            }
+
+            //encontrando datos de la recta 2
+
+            String[] datosEc2 = datosIntercepto(ec2);
+
+            Console.WriteLine(ec2+" "+ec1);
+
+            if (validarNumero(datosEc2[0]) == "error" || validarNumero(datosEc2[1]) == "error")
+            {
+                datosEc2 = datosPendiente(ec2);
+            }
+
+            if (validarNumero(datosEc1[0]) == "error" || validarNumero(datosEc1[1]) == "error" || validarNumero(datosEc2[0]) == "error" || validarNumero(datosEc2[1]) == "error")
+            {
+                Error();
+            }
+            else
+            {
+                float[] valoresEc1 = {0,0};
+                float[] valoresEc2 = {0,0};
+
+                valoresEc1[0] = float.Parse(datosEc1[0]);
+                valoresEc1[1] = float.Parse(datosEc1[1]);
+                valoresEc2[0] = float.Parse(datosEc2[0]);
+                valoresEc2[1] = float.Parse(datosEc2[1]);
+
+                Console.WriteLine(valoresEc1[0] + "  -> " + datosEc1[0]);
+
+                String caso = identidicarCaso(valoresEc1[0], valoresEc1[1], valoresEc2[0], valoresEc2[1]);
+
+
+                switch (caso) {
+                    case "paralelas":
+                        labelError.Text = "Las rectas: \n\t "+ ec1 +" y   \n\t"+ec2+"\n Son paralelas por lo que" +
+                            "no tienen punto de intersección";
+                        
+                        break;
+                    case "iguales":
+                        labelError.Text = "Las rectas: \n\t " + ec1 + " y    \n\t" + ec2 + "\n Son la misma recta por lo que" +
+                            "se intersectan en todos sus puntos";
+                        break;
+                    case "perpendiculares":
+                        float[] puntoInterseccionPerpendicular = { 0,0};
+                        puntoInterseccionPerpendicular = calcPuntoInterseccion(valoresEc1[0], valoresEc2[0], valoresEc1[1], valoresEc2[1]);
+
+                        labelError.Text = "Las rectas: \n\t " + ec1 + " y    \n\t" + ec2 + "\n Son perpendiculares y " +
+                            "su punto de intersección es\n\t ( " + puntoInterseccionPerpendicular[0] +" , "+puntoInterseccionPerpendicular[1]+" )";
+                        break;
+                    case "cruzan":
+                            float[] puntoInterseccionCruzado = {0,0};
+                            puntoInterseccionCruzado = calcPuntoInterseccion(valoresEc1[0], valoresEc2[0], valoresEc1[1], valoresEc2[1]);
+                            labelError.Text = "Las rectas: \n\t " + ec1 + " y    \n\t" + ec2 + "\n " +
+                            "\t se intersectan en el punto: \n\t ( " + puntoInterseccionCruzado[0] + " , " + puntoInterseccionCruzado[1] + " )";
+
+                        break;
+                    default: 
+                        Error();
+                        break;
+
+                }
+            }
+
+        }
+
+      
+
+        private void botonLimpiar_Click(object sender, EventArgs e)
+        {
+            reestablecer();
+        }
+
+        private void botonCalcular_Click(object sender, EventArgs e)
         {
             String ec1 = textBoxRecta1.Text;
-            ec1 = recortarString(ec1);
+            String ec2 = textBoxRecta2.Text;
 
-       
-   
+            ec1 = recortarString(ec1);
+            ec2 = recortarString(ec2);
+
+            calcularRectas(ec1,ec2);
         }
     }
 }
